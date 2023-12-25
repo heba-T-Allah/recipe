373742225d6499b2a6b6544c95bfbd9747ee8be9
heba <beboozain7@gmail.com>
2023-12-25 17:52:16 +0200

    update logo

--- lib/resources/assets_manager.dart
+++ lib/resources/assets_manager.dart
@@ -2,8 +2,7 @@ const String imagePath = "assets/images";
 
 class ImageAssets {
   static const String background = "$imagePath/background.png";
-  static const String logo = "$imagePath/logo.png";
-  static const String light1 = "$imagePath/light-1.png";
-  static const String light2 = "$imagePath/light-2.png";
+  static const String logo = "$imagePath/my_logo.png";
+
 
 }
